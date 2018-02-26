5b8a6219b8bf35add6926d4127e6c0f3f7b1f4ff
Max Johnson <maxrjohnson23@gmail.com>
2018-02-26 08:58:52 -0600

    Add closest friend logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,40 @@ apiRoutes.get("/friends", (req, res) => {
 });
 
 apiRoutes.post("/friends", (req, res) => {
+    const userInfo = req.body;
+    const userScores = userInfo.scores;
 
+    if (userScores) {
+        findClosestMatch(userScores);
+    } else {
+        res.status(500).send("Invalid user data");
+    }
+
+
+    //friendList.push(user);
+    res.send(userInfo);
 });
+
+
+function findClosestMatch(userScores) {
+    console.log(`User scores: ${userScores}`);
+    let closestMatch = friendList[0];
+    let closestScore = Infinity;
+    friendList.forEach(friend => {
+        let totalDifference = calcScoreDifference(friend.scores, userScores);
+        if (totalDifference < closestScore) {
+            closestScore = totalDifference;
+            closestMatch = friend;
+        }
+    });
+    console.log(`Closest friend: ${closestMatch.name}`);
+}
+
+function calcScoreDifference(friendScores, userScores) {
+    let scoreDifferences = friendScores.map((score, index) => {
+        return Math.abs(score - userScores[index]);
+    });
+    return scoreDifferences.reduce((curr, next) => curr + next, 0);
+}
 
 module.exports = apiRoutes;
