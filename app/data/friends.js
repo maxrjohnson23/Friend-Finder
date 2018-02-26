91db1c9e06b1192ddd96d5620c5b0fcdf66caea4
Max Johnson <maxrjohnson23@gmail.com>
2018-02-26 08:16:24 -0600

    Add sample friend data

---
+++ app/data/friends.js
@@ -0,0 +1,82 @@
+var friends = [
+    {
+        "name": "A",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "B",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            2,
+            4,
+            1,
+            5,
+            1,
+            5,
+            2,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "C",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            5,
+            4,
+            2,
+            5,
+            4,
+            2,
+            2,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "D",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            5,
+            4,
+            5,
+            1,
+            4,
+            5,
+            1,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "E",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            2,
+            4,
+            1,
+            5,
+            1,
+            5,
+            2,
+            4,
+            4
+        ]
+    }
+];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,11 @@
 const apiRoutes = require("express").Router();
 
+apiRoutes.get("/api/friends", (req, res) => {
 
+});
+
+apiRoutes.post("/api/friends", (req, res) => {
+    
+});
 
 module.exports = apiRoutes;
