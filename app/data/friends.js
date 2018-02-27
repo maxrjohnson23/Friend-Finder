d1538cf10df886ae06f674e400e5aa769c4eb266
Max <lfmax223@gmail.com>
2018-02-26 21:08:06 -0600

    Updated friend list for random generation

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,84 +1,49 @@
-let friendList = [
-    {
-        "name": "A",
-        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "B",
-        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
-        "scores": [
-            3,
-            2,
-            4,
-            1,
-            5,
-            1,
-            5,
-            2,
-            4,
-            4
-        ]
-    },
-    {
-        "name": "C",
-        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
-        "scores": [
-            1,
-            5,
-            4,
-            2,
-            5,
-            4,
-            2,
-            2,
-            1,
-            4
-        ]
-    },
-    {
-        "name": "D",
-        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
-        "scores": [
-            3,
-            5,
-            4,
-            5,
-            1,
-            4,
-            5,
-            1,
-            4,
-            2
-        ]
-    },
-    {
-        "name": "E",
-        "photo": "https://randomuser.me/api/portraits/men/83.jpg",
-        "scores": [
-            3,
-            2,
-            4,
-            1,
-            5,
-            1,
-            5,
-            2,
-            4,
-            4
-        ]
+const request = require('request');
+
+const options = {
+    hostname: 'randomuser.me',
+    port: 443,
+    path: '/api/?results=20',
+    method: 'GET'
+};
+
+let friendList = [];
+
+// Populate friends list with 20 random people
+function retrieveFriendList() {
+    request('https://www.randomuser.me/api?results=20&nat=us', function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            // Populate friends list
+            console.log(JSON.parse(body));
+            populateUsers(JSON.parse(body).results);
+            console.log(friendList);
+        }
+    });
+}
+
+function populateUsers(data) {
+    data.forEach(person => {
+        let user = {
+            name: `${person.name.first} ${person.name.last}`,
+            photo: person.picture.large,
+            scores: generateRandomScores()
+        };
+        friendList.push(user);
+    });
+}
+
+function generateRandomScores() {
+    let scores = [];
+    for (let i = 0; i < 10; i++) {
+        // random number between 1 and 5 (inclusive)
+        scores.push(Math.floor(Math.random() * (5)) + 1);
     }
-];
+    return scores;
+}
+
+retrieveFriendList();
+
 
 module.exports = friendList;
