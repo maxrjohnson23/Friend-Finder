d4a501925799dec02c530dd0178691ae0e9157d8
Max <lfmax223@gmail.com>
2018-02-26 19:25:25 -0600

    Updated survey page

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/app/public'));
 app.use('/', require('./app/routing/htmlRoutes'));
 app.use('/api', require('./app/routing/apiRoutes'));
 
